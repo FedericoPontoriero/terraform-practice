e7c3c91d34aeb4e4599d47d231459bde14ac9172
Federico <federicolucaspontoriero@gmail.com>
2023-09-09 00:52:56 -0300

    first commit

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.1"
+    }
+  }
+}
+
+# Configure AWS provider
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = "~/.aws/creds" # cualquier path
+  profile                  = "customprofile"
+}
